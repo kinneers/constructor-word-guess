584c9c6f8d72a68f9101e680282c5d6b763df677
Sarah Kinneer <kinneers@gmail.com>
2019-02-03 18:17:09 -0500

    Logic to determine correct and incorrect and decrement guesses correctly works, prompt to play again or exit works- working on logic to determine when word had been guessed correctly next

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ Randomly selects a word and uses the Word constructor to store it
 Prompts the user for each guess and keeps track of the user's remaining guesses*/
 
 var Word = require('./word.js');
+var Letter = require('./letter.js');
 var inquirer = require('inquirer');
 
 
@@ -21,6 +22,8 @@ function playGame() {
 //FOR TESTING- REMOVE BEFORE FINAL PUSH
     console.log(gameWord);
 
+    //Welcomes the user to the game
+    console.log("\nWelcome to the Word Guess Game!  Here is your first word:");
     //Displays the number of characters in the chosen word with underscores at the start of the game
     gameWord.display();
 
@@ -30,21 +33,34 @@ function playGame() {
             {
                 type: 'input',
                 message: 'Choose a Letter',
-                name: 'letter'
+                name: 'newGuess'
             }
         ]).then(function(res) {
-            character = res.letter.toLowerCase()
+            character = res.newGuess.toLowerCase()
             if (letterCheck.test(character)) {
-                if (guessesLeft > 0) {
-                    console.log('\nWorking');
+                if (guessesLeft >= 1) {
                     gameWord.makeGuess(character);
-                    //loop through the letter array and determine if the guess was correct or incorrect- console log it and increment guesses if needed
+                    //Boolean for if the guessed character is in the word
+                    var inWord = false;
+                    //Loop through the letter array and determine if the guess was correct or incorrect- console log result and decrement guesses if incorrect
+                    for (i = 0; i < gameWord.letters.length; i++) {
+                        if (character === gameWord.letters[i].letter) {
+                            inWord = true;
+                        }
+                    }
+                    if (inWord) {
+                        console.log('\nCORRECT!  :)');
+                    } else {
+                        guessesLeft--;
+                        console.log('\nINCORRECT!  :(\n\nYou have ' + guessesLeft + ' guesses remaining.');
+                    }
                     gameWord.display();
-                    promptUser();
-                } 
-                else if (guessesLeft === 0) {
-                    console.log("You ran out of guesses!");
-                    playAgain();
+                    if (guessesLeft === 0) {
+                        console.log("You ran out of guesses!");
+                        playAgain();
+                    } else {
+                        promptUser();
+                    }
                 } 
                 else {
                     console.log("Error");
@@ -55,8 +71,9 @@ function playGame() {
             }
         });
     }
-
+//Prompts use for first guess at the beginning of the game
 promptUser();
+}
 
 function playAgain() {
     inquirer.prompt([
@@ -73,8 +90,7 @@ function playAgain() {
             console.log("That was fun!  See you next time!");
         }
     });
+}
 
-}
-}
 
 playGame();
